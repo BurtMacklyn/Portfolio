4d55cbb2c08094c162ce7049eb5d23955d40d415
BurtMacklyn <cooperrunyanspam@gmail.com>
2021-12-06 20:03:34 -0700

    dev test 3

--- src/index/Main.tsx
+++ src/index/Main.tsx
@@ -1,27 +1,28 @@
-import React from 'react';
+// import React from 'react';
 
-import Header from './header/Header';
-import About from './about/About';
-import Experience from './experience/Experience';
-import Contact from './contact/Contact';
-import Links from './links/Links';
+// import Header from './header/Header';
+// import About from './about/About';
+// import Experience from './experience/Experience';
+// import Contact from './contact/Contact';
+// import Links from './links/Links';
 
-import MobileNav from '../components/MobileNav';
-import Footer from '../components/Footer';
-import Menu from '../components/Menu';
+// import MobileNav from '../components/MobileNav';
+// import Footer from '../components/Footer';
+// import Menu from '../components/Menu';
 
 function Main() {
   return (
-    <main className="index">
-      <MobileNav />
-      <Header />
-      <Menu active="index" />
-      <About />
-      <Experience />
-      <Links />
-      <Contact />
-      <Footer />
-    </main>
+    // <main className="index">
+    //   <MobileNav />
+    //   <Header />
+    //   <Menu active="index" />
+    //   <About />
+    //   <Experience />
+    //   <Links />
+    //   <Contact />
+    //   <Footer />
+    // </main>
+    <p>hi</p>
   );
 }
 
