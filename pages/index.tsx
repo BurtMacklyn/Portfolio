1ba1db6a85856fa72a4eb57fec77eff44c523cb7
BurtMacklyn <cooperrunyanspam@gmail.com>
2021-12-06 22:34:42 -0700

    title test

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,19 @@ import React from 'react';
 import Index from '@index/Main';
 import Motion from '@components/Motion';
 
+import Head from 'next/head';
+
 export const config = { unstable_runtimeJS: false };
 
 export default function Main(): JSX.Element {
   return (
-    <Motion>
-      <Index />
-    </Motion>
+    <>
+      <Head>
+        <title>TEST</title>
+      </Head>
+      <Motion>
+        <Index />
+      </Motion>
+    </>
   );
 }
