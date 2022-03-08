d47f193068305f6c99f332193fb19403dd4481aa
Cooper Runyan <cooperrunyanspam@gmail.com>
2022-03-08 09:14:43 -0700

    Quote section

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,13 @@
 import { Portfolio, Nav } from '~/components';
 import { Header } from './Header';
+import { Quote } from './Quote';
 
 export function Home() {
   return (
     <Portfolio>
       <Nav />
       <Header />
+      <Quote />
     </Portfolio>
   );
 }
---
+++ src/pages/Home/Quote/Quote.tsx
@@ -0,0 +1,16 @@
+import { Quotation } from './Quotation';
+import style from './Quote.module.scss';
+
+export function Quote() {
+  return (
+    <div className={style.Quote}>
+      <Quotation className={style.Quotation} />
+      <h3 className={style.text}>Any application that can be written in JavaScript, will eventually be written in JavaScript.</h3>
+      <div className={style.cite}>
+        <p className={style.name}>Jeff Atwood</p>
+        <p className={style.description}>Creator of Stack Overflow</p>
+        <span className={style.circle}></span>
+      </div>
+    </div>
+  );
+}
