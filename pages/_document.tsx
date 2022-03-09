66be03f2fd4d61ca1c2c38383aed18d24a5f94c3
Cooper Runyan <cooperrunyanspam@gmail.com>
2022-03-08 23:53:50 -0700

    pwa assets

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,16 +5,18 @@ export default function Document(): JSX.Element {
   return (
     <Html lang="en">
       <Head>
-        <link rel="shortcut icon" type="image/x-icon" href="./favicon.ico" />
-        <link rel="manifest" href="./manifest.json" />
+        <link rel="shortcut icon" type="image/x-icon" href="/pwa/favicon.ico" />
         <base href="/" />
+
+        <link rel="apple-touch-icon" href="/pwa/apple-touch-icon.png" />
+        <link rel="manifest" href="./manifest.json" />
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"></link>
 
-        <link rel="apple-touch-icon" href="./icons/apple-icon-180.png" />
-        <link rel="apple-touch-icon" href="icons/apple-icon-180.png" />
+        <link rel="apple-touch-icon" href="./pwa/apple-icon-180.png" />
+        <link rel="apple-touch-icon" href="./pwa/apple-icon-180.png" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
 
         <meta name="theme-color" content="#fff" />
