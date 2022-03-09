bbc62346bd911506275b9f7ff4c542d3a62003f3
Cooper Runyan <cooperrunyanspam@gmail.com>
2022-03-09 09:34:42 -0700

    404 page title

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -2,20 +2,27 @@ import style from './NotFound.module.scss';
 import { Graphic } from './Graphic';
 import { useRouter } from 'next/router';
 import { Back } from './Back';
+import Head from 'next/head';
+import { Portfolio } from '~/components';
 
 export function NotFound() {
   const router = useRouter();
 
   return (
-    <div className={style.NotFound}>
-      <Graphic className={style.Graphic} />
-      <div className={style.content}>
-        <h1>404</h1>
-        <h2>Page not found</h2>
-        <button onClick={() => router.back()}>
-          <Back /> Back
-        </button>
+    <Portfolio>
+      <Head>
+        <title>404: Page not found</title>
+      </Head>
+      <div className={style.NotFound}>
+        <Graphic className={style.Graphic} />
+        <div className={style.content}>
+          <h1>404</h1>
+          <h2>Page not found</h2>
+          <button onClick={() => router.back()}>
+            <Back /> Back
+          </button>
+        </div>
       </div>
-    </div>
+    </Portfolio>
   );
 }
