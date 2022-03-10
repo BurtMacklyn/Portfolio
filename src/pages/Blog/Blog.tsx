60e97e897e0af82d4ae327d1fa5e48d8cd84064c
Cooper Runyan <cooperrunyanspam@gmail.com>
2022-03-09 20:33:46 -0700

    blog main page

--- src/pages/Blog/Blog.tsx
+++ src/pages/Blog/Blog.tsx
@@ -1,24 +1,31 @@
 import style from './Blog.module.scss';
 import { Link, Portfolio, Nav, Center } from '~/components';
 
-type Data = {
-  title: string;
-  preview: string;
-};
+function sort(from: string[], data: any) {
+  const s: string[] = [];
+  from
+    .sort((a, b) => {
+      return new Date(data[a].timestamp).getTime() - new Date(data[b].timestamp).getTime();
+    })
+    .forEach((path) => s.unshift(path));
+  return s;
+}
 
-export function Blog({ pages, data }: { pages: string[]; data: Data }) {
-  console.log(data);
-
+export function Blog({ pages, data }: { pages: string[]; data: any }) {
   return (
     <Portfolio>
       <Nav />
       <Center>
         <div className={style.section}>
-          {pages.map((page) => (
-            <Link key={page} href={page}>
-              {page}
-            </Link>
-          ))}
+          <h2>Blog</h2>
+          <div className={style.content}>
+            {sort(pages, data).map((page) => (
+              <Link key={page} href={page} className={style.Card}>
+                <img src={data[page]?.preview} alt="Preview image" />
+                <p>{data[page]?.title}</p>
+              </Link>
+            ))}
+          </div>
         </div>
       </Center>
     </Portfolio>
