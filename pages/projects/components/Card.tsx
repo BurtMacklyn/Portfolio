3ad8162f478c363e871d09e12f0850e19716fe54
Cooper Runyan <cooperrunyanspam@gmail.com>
2022-03-17 17:26:34 -0600

    responsive spacing

--- pages/projects/components/Card.tsx
+++ pages/projects/components/Card.tsx
@@ -2,8 +2,11 @@ import style from '~/style/components/Projects.module.scss';
 import Image from 'next/image';
 import type { Project } from '~/types';
 import { Link } from '~/components';
+import { useMediaQuery } from 'usehooks-ts';
 
 export function Card({ children: project }: { children: Project }) {
+  const matches = useMediaQuery('(min-width: calc(700em / 16))');
+
   return (
     <Link href={project.link} className={style.card} key={project.name}>
       <div className={style.content}>
@@ -23,7 +26,20 @@ export function Card({ children: project }: { children: Project }) {
         </div>
       </div>
 
-      <Image layout="fixed" width={240} height={240} src={project.preview} alt={project.name} />
+      <>
+        {matches && (
+          <div>
+            <Image layout="fixed" width={240} height={240} src={project.preview} alt={project.name} />
+          </div>
+        )}
+      </>
+      <>
+        {!matches && (
+          <div>
+            <Image layout="responsive" width={2400} height={2400} src={project.preview} alt={project.name} />
+          </div>
+        )}
+      </>
     </Link>
   );
 }
