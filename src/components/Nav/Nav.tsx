38f0fc86604e2218d57d020fd506a631125187fd
Cooper Runyan <cooperrunyanspam@gmail.com>
2022-03-18 01:17:04 -0600

    mobile nav

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,12 +1,21 @@
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import { Signature, Link } from '~/components';
 import { useObserver } from '~/hooks';
 import { LogoCircle } from './LogoCircle';
 import style from './Nav.module.scss';
+import MobileSVG from './Mobile';
+import X from './X';
+import { useMediaQuery } from 'usehooks-ts';
 
 export function Nav() {
   const dummy = useRef<HTMLDivElement>(null);
   const active = useObserver(dummy);
+  const check = useRef<HTMLInputElement>(null);
+  const matches = useMediaQuery('(max-width: calc(900em / 16))');
+
+  useEffect(() => {
+    if (check.current && check.current.checked && !matches) check.current.checked = false;
+  }, [matches]);
 
   return (
     <>
@@ -18,7 +27,10 @@ export function Nav() {
             <Link href="/">
               <Signature color="white" className={style.Signature} />
             </Link>
-            <ul>
+            <label htmlFor="mobile" className={style.mobileButton}>
+              <MobileSVG />
+            </label>
+            <ul className={style.desktopLinks}>
               <li>
                 <Link href="/projects">Projects</Link>
               </li>
@@ -34,6 +46,30 @@ export function Nav() {
             </ul>
           </div>
         </div>
+      </div>
+
+      <input ref={check} type="checkbox" className={style.check} name="mobile" id="mobile" style={{ display: 'none' }} />
+      <div className={style.mobile}>
+        <div className={style.top}>
+          <Signature color="black" />
+          <label htmlFor="mobile" className={style.X}>
+            <X />
+          </label>
+        </div>
+        <ul className={style.mobileLinks}>
+          <li>
+            <Link href="/projects">Projects</Link>
+          </li>
+          <li>
+            <Link href="/blog">Blog</Link>
+          </li>
+          <li>
+            <Link href="/play">Games</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
       </div>
     </>
   );
---
+++ src/components/Nav/X.tsx
@@ -0,0 +1,8 @@
+export default function SVG() {
+  return (
+    <svg height="30" viewBox="0 0 72 72" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <line x1="2.82843" y1="3" x2="69" y2="69.1716" stroke="black" strokeWidth="4px" strokeLinecap="round" />
+      <line x1="69" y1="2.82843" x2="2.82843" y2="69" stroke="black" strokeWidth="4px" strokeLinecap="round" />
+    </svg>
+  );
+}
