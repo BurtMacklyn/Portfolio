ff4d081a483a0cd7859317e8b8900b225ea41f79
Cooper Runyan <cooperrunyanspam@gmail.com>
2022-03-18 14:48:21 -0600

    fixed tables

--- src/components/Markdown/Markdown.tsx
+++ src/components/Markdown/Markdown.tsx
@@ -1,12 +1,30 @@
+import { useEffect, useRef } from 'react';
 import ReactMarkdown from 'react-markdown';
 import type { ReactMarkdownOptions } from 'react-markdown/lib/react-markdown';
 import remarkGfm from 'remark-gfm';
 import style from './Markdown.module.scss';
 
 export function Markdown({ children }: ReactMarkdownOptions) {
+  const article = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (article.current) {
+      article.current.innerHTML = article.current.innerHTML
+        .replaceAll(/<table/gi, '<div class="table-wrapper"><table')
+        .replaceAll(/<\/table>/gi, '</table></div>');
+
+      for (const tableWrapper of Array.from(document.querySelectorAll('.table-wrapper') as NodeListOf<HTMLDivElement>)) {
+        tableWrapper.style.width = '100%';
+        tableWrapper.style.overflow = 'scroll';
+        (tableWrapper.children[0]! as HTMLTableElement).style!.width = 'max-content';
+        (tableWrapper.children[0]! as HTMLTableElement).style!.minWidth = '100%';
+      }
+    }
+  }, [article.current]);
+
   return (
-    <div className={style.article}>
+    <article ref={article} className={style.article}>
       <ReactMarkdown remarkPlugins={[remarkGfm]}>{children}</ReactMarkdown>
-    </div>
+    </article>
   );
 }
