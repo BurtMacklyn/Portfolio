edc1366f0c99ba4d5827bdacab2d2da6d571eddb
cooperrunyan <cooperrunyanspam@gmail.com>
2022-04-27 23:42:31 -0700

    Added google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,16 @@
 import 'src/style/base.scss';
 import Head from 'next/head';
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`} />
+      <Script strategy="lazyOnload" id="google-analytics">
+        {`window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');`}
+      </Script>
+
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, minimum-scale=1" />
       </Head>
