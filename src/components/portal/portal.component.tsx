8bcb0f8b6e92fd4bc5e053bec5fc3322a1172ece
cooperrunyan <cooperrunyanspam@gmail.com>
2022-05-06 18:37:42 -0700

    Portal

--- src/components/portal/portal.component.tsx
+++ src/components/portal/portal.component.tsx
@@ -1,18 +1,21 @@
 // eslint-disable-next-line no-use-before-define
 import type React from 'react';
 import { Back } from '~/components/back/back.component';
+import { Portfolio } from '../portfolio/portfolio.component';
 
 import style from './portal.module.scss';
 
 export const Portal: React.FC<{ url: URL; noBack?: true }> = ({ url, noBack }) => {
   return (
-    <div className={style.Portal}>
-      {!noBack && (
-        <span className={style.Back}>
-          <Back arrow color="white" />
-        </span>
-      )}
-      <iframe src={url.href} frameBorder="0" />
-    </div>
+    <Portfolio>
+      <div className={style.Portal}>
+        {!noBack && (
+          <span className={style.Back}>
+            <Back arrow color="white" />
+          </span>
+        )}
+        <iframe src={url.href} frameBorder="0" />
+      </div>
+    </Portfolio>
   );
 };
