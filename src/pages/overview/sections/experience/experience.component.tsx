630651af770c0b23dbe337acea2b3a5a5b167fac
cooperrunyan <cooperrunyanspam@gmail.com>
2022-05-09 17:16:54 -0700

    Add logos

--- src/pages/overview/sections/experience/experience.component.tsx
+++ src/pages/overview/sections/experience/experience.component.tsx
@@ -4,7 +4,7 @@ import * as icons from '~/images/experience/index';
 export const Experience = () => (
   <div className={style.wrapper}>
     <div className={style.Experience}>
-      {Object.entries({ ...icons }).map(([key, Icon]) => (
+      {Object.entries(icons).map(([key, Icon]) => (
         <div className={style.card} key={key}>
           <Icon />
         </div>
