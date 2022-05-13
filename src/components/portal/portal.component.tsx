c942fe3f2f637053fbe1efac3f62fdc2f5187197
cooperrunyan <cooperrunyanspam@gmail.com>
2022-05-13 12:07:03 -0700

    portal overscroll behavior

--- src/components/portal/portal.component.tsx
+++ src/components/portal/portal.component.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import type React from 'react';
 import { Back } from '~/components/back/back.component';
 import { Portfolio } from '../portfolio/portfolio.component';
@@ -6,6 +7,9 @@ import style from './portal.module.scss';
 
 export const Portal: React.FC<{ url: URL; noBack?: true }> = ({ url, noBack }) => (
   <Portfolio>
+    <Head>
+      <style dangerouslySetInnerHTML={{ __html: `body { overscroll-behavior-y: none }` }} />
+    </Head>
     <div className={style.Portal}>
       {!noBack && (
         <span className={style.Back}>
