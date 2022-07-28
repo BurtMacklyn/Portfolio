b900f96c2d5b93c90be8ac04858e104bddf72f61
cooperrunyan <cooperrunyanspam@gmail.com>
2022-07-27 20:06:27 -0700

    Make About section temporal based

--- islands/About.tsx
+++ islands/About.tsx
@@ -8,8 +8,8 @@ export default function About() {
     <section class={tw`my-16`}>
       <h2 class={tw`lowercase font-semibold text-5xl tracking-tighter leading-none`}>About.</h2>
       <p class={tw`font-normal leading-relaxed tracking-tight mt-6 w-full max-w-[42rem] text-g80`}>
-        I'm a <span class={tw`text-white`}>2 year fullstack software developer</span> specializing in frontend web design/development and backend architecture
-        with technologies like HTML, NodeJS, Deno, Docker, Go, NextJS, React, SCSS, and Typescript.
+        I'm a <span class={tw`text-white`}>{new Date().getFullYear() - 2019} year fullstack software developer</span> specializing in frontend web
+        design/development and backend architecture with technologies like HTML, NodeJS, Deno, Docker, Go, NextJS, React, SCSS, and Typescript.
       </p>
     </section>
   );
