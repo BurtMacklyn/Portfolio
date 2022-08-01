ea8f88bf1bf22682620d5c5e0402a577011a8ea2
cooperrunyan <cooperrunyanspam@gmail.com>
2022-07-31 21:21:00 -0700

    Add theme meta tag

--- components/Head.tsx
+++ components/Head.tsx
@@ -8,6 +8,7 @@ export const Head = () => {
       <title>Cooper Runyan</title>
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <meta name="theme" content="#000" />
       <link
         href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Roboto+Mono:wght@100;200;300;400;500;600;700&display=swap"
         rel="stylesheet"
