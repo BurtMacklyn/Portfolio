3acfd92d79a0aaf28f811eed95dcf2e3de459cd2
cooperrunyan <cooperrunyanspam@gmail.com>
2022-08-01 17:44:30 -0700

    Add config variables

--- islands/Footer.tsx
+++ islands/Footer.tsx
@@ -2,21 +2,18 @@
 
 import { h } from 'preact';
 import { tw } from '@twind';
-import { margin, maxWidth } from '../lib/config.ts';
+import { lastUpdated, margin, maxWidth, version } from '../lib/config.ts';
 
 export default function Footer() {
   return (
     <footer class={tw`bg-g4 text-g80`}>
       <div class={tw`${margin} mx-auto py-6 max-w-[${maxWidth}] flex flex-col gap-2`}>
+        <p class={tw`text-sm`}>Last updated: {lastUpdated}</p>
         <p class={tw`text-sm`}>
-          <Name /> Portfolio v5.2.6
+          {name} Portfolio v{version}
         </p>
-        <p class={tw`text-sm`}>
-          Copyright © 2022 - All rights reserved by <Name />.
-        </p>
-        <p class={tw`text-sm`}>
-          Site created and designed by <Name /> using Deno and Fresh (React)
-        </p>
+        <p class={tw`text-sm`}>Copyright © 2022 - All rights reserved by {name}.</p>
+        <p class={tw`text-sm`}>Site created and designed by {name} using Deno and Fresh (React)</p>
         <p class={tw`text-sm`}>
           View source code{' '}
           <a class={tw`text-secondary`} href="https://www.github.com/cooperrunyan/cooperrunyan" aria-content="View repository">
@@ -29,7 +26,7 @@ export default function Footer() {
   );
 }
 
-const Name = () => (
+const name = (
   <a class={tw`text-sm text-secondary`} href="https://www.github.com/cooperrunyan">
     Cooper Runyan
   </a>
--- lib/config.ts
+++ lib/config.ts
@@ -2,3 +2,6 @@ export const maxWidth = '112.5rem'; // 1800px
 export const margin = 'xl:px-24 lg:px-16 md:px-12 sm:px-8 xs:px-4 mxl:px-32';
 export const emailAddress = 'cooperrunyan@gmail.com';
 export const email = `mailto:${emailAddress}`;
+
+export const lastUpdated = 'August 1, 2022 5:39 PM MST';
+export const version = '5.2.6';
