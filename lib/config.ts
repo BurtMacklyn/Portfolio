fa25dcbea126e45fd3031fe8c3f40c6fb95fca81
cooperrunyan <cooperrunyanspam@gmail.com>
2022-08-09 10:12:48 -0700

    Update config

--- lib/config.ts
+++ lib/config.ts
@@ -3,5 +3,5 @@ export const margin = 'xl:px-24 lg:px-16 md:px-12 sm:px-8 xs:px-4 mxl:px-32';
 export const emailAddress = 'cooperrunyan@gmail.com';
 export const email = `mailto:${emailAddress}`;
 
-export const lastUpdated = 'August 8, 2022 8:24 PM MST';
-export const version = '5.4.2';
+export const lastUpdated = 'August 9, 2022 10:12 PM MST';
+export const version = '5.6.1';
