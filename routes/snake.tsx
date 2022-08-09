2f37eac7ccdd6fd398703c0a2b32b7ee3acabc3b
cooperrunyan <cooperrunyanspam@gmail.com>
2022-08-09 10:12:18 -0700

    Add screen media query

--- routes/snake.tsx
+++ routes/snake.tsx
@@ -5,10 +5,13 @@ import { config, tw } from 'style';
 import Layout from 'c/Layout.tsx';
 
 import { useEffect, useRef } from 'preact/hooks';
+import { useMediaQuery } from '../hooks/useMediaQuery.ts';
 
 const theme = config.theme?.colors as any;
 
 export default function Snake() {
+  const matches = useMediaQuery('only screen');
+
   const ref = useRef<HTMLIFrameElement>(null);
 
   useEffect(() => {
@@ -22,13 +25,20 @@ export default function Snake() {
 
   return (
     <Layout noFooter>
-      <iframe
-        ref={ref}
-        class={tw`h-[calc(100vh-11rem)] border-1 border-g8 rounded`}
-        src={`https://cooperrunyan-snake.netlify.app/?showScore=false&primary=${encodeURIComponent(theme.secondary)}&secondary=${encodeURIComponent(
-          theme.primary,
-        )}&black=${encodeURIComponent(theme.black)}&white=${encodeURIComponent(theme.white)}&autoRestart=true`}
-      />
+      {!matches && (
+        <iframe
+          ref={ref}
+          class={tw`h-[calc(100vh-11rem)] border-1 border-g8 rounded`}
+          src={`https://cooperrunyan-snake.netlify.app/?showScore=false&primary=${encodeURIComponent(theme.secondary)}&secondary=${encodeURIComponent(
+            theme.primary,
+          )}&black=${encodeURIComponent(theme.black)}&white=${encodeURIComponent(theme.white)}&autoRestart=true`}
+        />
+      )}
+      {matches && (
+        <div class={tw`mt-12`}>
+          <h1 class={tw`font-bold text-7xl sm:text-6xl xxs:text-5xl tracking-tighter leading-none`}>Your device is not supported for snake</h1>
+        </div>
+      )}
     </Layout>
   );
 }
