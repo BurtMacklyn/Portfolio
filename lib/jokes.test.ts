6d01554229d6afb03d20a7a2222729d188eb5626
cooperrunyan <cooperrunyanspam@gmail.com>
2022-08-10 09:12:17 -0700

    Typo

--- lib/jokes.test.ts
+++ lib/jokes.test.ts
@@ -2,7 +2,7 @@ import * as t from 'testing';
 
 import { jokes } from './jokes.ts';
 
-Deno.test('is an array of jokes', async ({ step }) => {
+Deno.test('is an array of jokes', async () => {
   t.assertInstanceOf(jokes, Array, 'Jokes is an array');
   t.assertExists(jokes[0], 'Jokes contains at least one joke');
 });
--- lib/technologies.test.ts
+++ lib/technologies.test.ts
@@ -2,7 +2,7 @@ import * as t from 'testing';
 
 import { technologies } from './technologies.ts';
 
-Deno.test('is an array of technologies', async ({ step }) => {
+Deno.test('is an array of technologies', async () => {
   t.assertInstanceOf(technologies, Array, 'Technologies is an array');
   t.assertExists(technologies[0], 'Technologies contains at least one technologies');
 });
