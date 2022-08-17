0b2ded16e4f84d778344fcc706a397bde262ac52
Cooper Runyan <73623839+cooperrunyan@users.noreply.github.com>
2022-08-17 13:49:23 -0700

    Update config.ts

--- lib/config.ts
+++ lib/config.ts
@@ -3,5 +3,5 @@ export const margin = 'xl:px-24 lg:px-16 md:px-12 sm:px-8 xs:px-4 mxl:px-32';
 export const emailAddress = 'cooperrunyan@gmail.com';
 export const email = `mailto:${emailAddress}`;
 
-export const lastUpdated = 'August 10, 2022 9:12 AM MST';
-export const version = '5.621';
+export const lastUpdated = 'August 17, 2022 1:49 PM MST';
+export const version = '5.6.22';
