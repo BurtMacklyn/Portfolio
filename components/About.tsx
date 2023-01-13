41b2a1da38468a5be26823be759122ac798bcc41
Cooper <cooperrunyanspam@gmail.com>
2023-01-12 21:43:34 -0700

    Update About.tsx

--- components/About.tsx
+++ components/About.tsx
@@ -8,7 +8,7 @@ export default function About() {
     <section class={tw`my-16`}>
       <SectionTitle>About</SectionTitle>
       <p class={tw`font-normal leading-relaxed tracking-tight mt-6 w-full max-w-[42rem] text-g80`}>
-        I'm a <span class={tw`text-white font-bold`}>{new Date().getFullYear() - 2019} year fullstack software developer</span> specializing in frontend web
+        I'm a <span class={tw`text-white font-bold`}>{new Date().getFullYear() - 2021} year fullstack software developer</span> specializing in frontend web
         design/development and backend architecture with technologies like HTML, NodeJS, Deno, Docker, Go, NextJS, React, SCSS, and Typescript.
       </p>
     </section>
