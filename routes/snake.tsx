ffcff007229d09adde488cef1a4f57706b0ee5ad
Cooper <c@cooperrunyan.com>
2023-01-20 11:07:50 -0700

    Update snake.tsx

--- routes/snake.tsx
+++ routes/snake.tsx
@@ -8,18 +8,7 @@ import Layout from 'c/Layout.tsx';
 export default function Snake() {
   return (
     <Layout noFooter>
-      <iframe
-        class={tw`h-[calc(100vh-11rem)] border-1 border-grey8 rounded`}
-        src={`https://aisnake.netlify.app/?primary=${encodeURIComponent(
-          config.colors.white,
-        )}&secondary=${encodeURIComponent(
-          config.colors.primary,
-        )}&bg=${encodeURIComponent(
-          config.colors.black,
-        )}&fg=${encodeURIComponent(
-          config.colors.white,
-        )}&tickRate=10&unitSize=30`}
-      />
+     
     </Layout>
   );
 }
