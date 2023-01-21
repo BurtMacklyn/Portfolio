52a808fca73e7753fb3ecefbaa4e4cf28a88beae
cooperrunyan <c@cooperrunyan.com>
2023-01-21 02:38:17 -0700

    Code coverage improvements

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,7 +4,7 @@ import { font } from '@/config/font';
 import { style, Z } from '@/config/style';
 import { opacity, rem } from '@/css';
 import NextLink from 'next/link';
-import { CSSProperties, useEffect, useRef, useState } from 'react';
+import { CSSProperties, forwardRef, useEffect, useRef, useState } from 'react';
 
 export interface ButtonProps {
   children?: any;
@@ -25,7 +25,7 @@ export interface ButtonProps {
 }
 
 export const Button: React.FC<ButtonProps> = props => {
-  const ref = useRef<HTMLDivElement>(null);
+  const ref = useRef<HTMLElement>(null);
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
 
@@ -40,165 +40,201 @@ export const Button: React.FC<ButtonProps> = props => {
   }, [props.target]);
 
   return (
-    <DetermineButtonElement {...props}>
-      <Box
-        ref={ref}
-        relative
-        row
-        round
-        paddingBlock={12}
-        paddingInline={16 as any}
-        style={{
+    <DetermineButtonElement
+      ref={ref}
+      onClick={props.onClick}
+      href={props.href}
+      new={props.new}
+      data-testid={(props as any)['data-testid']}
+      style={
+        {
+          display: 'flex',
+          textDecoration: 'none',
+          borderRadius: style.borderRadius,
+          cursor: 'pointer',
+          userSelect: 'none',
+          position: 'relative',
+          paddingBlock: rem(12),
+          paddingInline: rem(16),
           width: props.fillX ? '100%' : 'fit-content',
           height: props.fillY ? '100%' : undefined,
-          // border: `${rem(2)} solid ${style.color[8]}`,
+        } as CSSProperties
+      }
+      onMouseMove={
+        props.target || props.static
+          ? undefined
+          : (e: any) => {
+              const rect = ref.current!.getBoundingClientRect();
+              setX(e.clientX - rect.left);
+              setY(e.clientY - rect.top);
+            }
+      }
+      onMouseOver={props.static ? undefined : () => setHover(true)}
+      onMouseOut={props.static ? undefined : () => setHover(false)}>
+      <Content
+        {...{
+          elhover: hover,
+          x,
+          y,
+          children: props.children,
+          animationSpeedModifier: props.animationSpeedModifier,
+          hover: props.hover,
+          large: props.large,
+          raw: props.raw,
+          static: props.static,
         }}
-        raw={{
-          onMouseMove:
-            props.target || props.static
-              ? undefined
-              : e => {
-                  const rect = ref.current!.getBoundingClientRect();
-                  setX(e.clientX - rect.left);
-                  setY(e.clientY - rect.top);
-                },
-          onMouseOver: props.static ? undefined : () => setHover(true),
-          onMouseOut: props.static ? undefined : () => setHover(false),
-        }}>
-        {/* Border */}
+      />
+    </DetermineButtonElement>
+  );
+};
+
+interface ContentProps {
+  children?: any;
+  x: number;
+  y: number;
+  hover?: boolean;
+  elhover: boolean;
+  raw?: CSSProperties;
+  large?: boolean;
+  animationSpeedModifier?: number;
+  static?: boolean;
+}
+
+const Content: React.FC<ContentProps> = ({ x, y, ...props }) => {
+  return (
+    <>
+      {/* Border */}
+      <Box
+        w="fill"
+        h="fill"
+        style={{
+          background: style.color[12],
+          paddingBlock: 'inherit',
+          paddingInline: 'inherit',
+          position: 'absolute',
+          zIndex: Z.Elevated,
+          top: 0,
+          left: 0,
+          bottom: 0,
+          right: 0,
+          opacity: 1,
+          borderRadius: 'inherit',
+        }}
+      />
+
+      {/* Border Circle */}
+      {!props.static && (
         <Box
+          smooth
+          animationSpeedModifier={props.animationSpeedModifier || 2}
           w="fill"
           h="fill"
           style={{
-            background: style.color[12],
+            background: `radial-gradient(${
+              props.large ? 300 : 75
+            }px circle at ${x}px ${y}px, ${opacity(
+              style.color.primary,
+              60,
+            )}, transparent)`,
             paddingBlock: 'inherit',
             paddingInline: 'inherit',
             position: 'absolute',
             zIndex: Z.Elevated,
             top: 0,
             left: 0,
-            bottom: 0,
-            right: 0,
-            opacity: 1,
+            opacity: (props.hover !== undefined ? props.hover : props.elhover)
+              ? 1
+              : 0,
             borderRadius: 'inherit',
+            transitionProperty: 'all',
           }}
         />
+      )}
 
-        {/* Border Circle */}
+      <Box
+        style={{
+          zIndex: Z.Elevated2,
+          position: 'absolute',
+          top: rem(1),
+          left: rem(1),
+          bottom: rem(1),
+          right: rem(1),
+          background: style.color[0],
+          borderRadius: 'inherit',
+          padding: 'inherit',
+        }}>
         {!props.static && (
           <Box
             smooth
             animationSpeedModifier={props.animationSpeedModifier || 2}
-            w="fill"
-            h="fill"
             style={{
               background: `radial-gradient(${
-                props.large ? 300 : 75
+                props.large ? 600 : 200
               }px circle at ${x}px ${y}px, ${opacity(
                 style.color.primary,
-                60,
-              )}, transparent)`,
-              paddingBlock: 'inherit',
-              paddingInline: 'inherit',
-              position: 'absolute',
-              zIndex: Z.Elevated,
+                props.large ? 20 : 25,
+              )}, transparent 40%)`,
+
+              opacity: props.elhover ? 1 : 0,
               top: 0,
               left: 0,
-              opacity: (props.hover !== undefined ? props.hover : hover)
-                ? 1
-                : 0,
+              bottom: 0,
+              right: 0,
+              position: 'absolute',
+              zIndex: Z.Elevated3,
               borderRadius: 'inherit',
+              padding: 'inherit',
               transitionProperty: 'all',
             }}
           />
         )}
-
-        <Box
-          style={{
-            zIndex: Z.Elevated2,
-            position: 'absolute',
-            top: rem(1),
-            left: rem(1),
-            bottom: rem(1),
-            right: rem(1),
-            background: style.color[0],
-            borderRadius: 'inherit',
-            padding: 'inherit',
-          }}>
-          {!props.static && (
-            <Box
-              smooth
-              animationSpeedModifier={props.animationSpeedModifier || 2}
-              style={{
-                background: `radial-gradient(${
-                  props.large ? 600 : 200
-                }px circle at ${x}px ${y}px, ${opacity(
-                  style.color.primary,
-                  props.large ? 20 : 25,
-                )}, transparent 40%)`,
-
-                opacity: hover ? 1 : 0,
-                top: 0,
-                left: 0,
-                bottom: 0,
-                right: 0,
-                position: 'absolute',
-                zIndex: Z.Elevated3,
-                borderRadius: 'inherit',
-                padding: 'inherit',
-                transitionProperty: 'all',
-              }}
-            />
-          )}
-        </Box>
-
-        <div
-          style={{
-            position: 'relative',
-            zIndex: Z.Elevated4,
-
-            fontFamily: font.sans.family,
-            fontWeight: font.sans.bold,
-            fontSize: rem(16),
-            width: '100%',
-            maxWidth: rem(640),
-            letterSpacing: '-0.02em',
-          }}>
-          {props.children}
-        </div>
       </Box>
-    </DetermineButtonElement>
+
+      <div
+        style={{
+          position: 'relative',
+          zIndex: Z.Elevated4,
+
+          fontFamily: font.sans.family,
+          fontWeight: font.sans.bold,
+          fontSize: rem(16),
+          width: '100%',
+          maxWidth: rem(640),
+          letterSpacing: '-0.02em',
+        }}>
+        {props.children}
+      </div>
+    </>
   );
 };
 
-const DetermineButtonElement: React.FC<ButtonProps> = props => {
-  const s = {
-    display: 'flex',
-    textDecoration: 'none',
-    borderRadius: style.borderRadius,
-    cursor: 'pointer',
-    userSelect: 'none',
-    width: 'fit-content',
-    ...props.raw,
-  } as const;
-
+const DetermineButtonElement: React.FC<any> = forwardRef((props, ref) => {
   if (props.href)
     return (
       <NextLink
+        ref={ref as any}
         data-testid={(props as any)['data-testid']}
-        style={s}
+        style={props.style}
         href={props.href}
+        onMouseMove={props.onMouseMove}
+        onMouseEnter={props.onMouseEnter}
+        onMouseLeave={props.onMouseLeave}
         target={props.new ? '_blank' : undefined}>
         {props.children}
       </NextLink>
     );
+
   return (
     <PlainButton
+      ref={ref as any}
       data-testid={(props as any)['data-testid']}
-      style={s}
+      style={props.style}
+      onMouseMove={props.onMouseMove}
+      onMouseEnter={props.onMouseEnter}
+      onMouseLeave={props.onMouseLeave}
       onClick={props.onClick}
       stopPropagation>
       {props.children}
     </PlainButton>
   );
-};
+});
--- src/components/Button/PlainButton.tsx
+++ src/components/Button/PlainButton.tsx
@@ -1,5 +1,5 @@
 import { BoxProps } from '@/components/Box';
-import { CSSProperties } from 'react';
+import { CSSProperties, forwardRef } from 'react';
 
 interface PlainButtonProps {
   children?: any;
@@ -7,27 +7,36 @@ interface PlainButtonProps {
   onClick?: () => any;
   stopPropagation?: boolean;
   style?: CSSProperties;
+  onMouseMove?: any;
+  onMouseEnter?: any;
+  onMouseLeave?: any;
 }
 
-export const PlainButton: React.FC<PlainButtonProps> = props => {
-  return (
-    <button
-      data-testid={(props as any)['data-testid']}
-      onClick={e => {
-        e.preventDefault();
-        if (props.stopPropagation) e.stopPropagation();
-        if (props.onClick) props.onClick();
-      }}
-      style={{
-        border: 0,
-        outline: 0,
-        padding: 0,
-        margin: 0,
-        cursor: 'pointer',
-        background: 'transparent',
-        ...props.style,
-      }}>
-      {props.children}
-    </button>
-  );
-};
+export const PlainButton = forwardRef<HTMLButtonElement, PlainButtonProps>(
+  (props, ref) => {
+    return (
+      <button
+        ref={ref}
+        data-testid={(props as any)['data-testid']}
+        onClick={e => {
+          e.preventDefault();
+          if (props.stopPropagation) e.stopPropagation();
+          if (props.onClick) props.onClick();
+        }}
+        style={{
+          border: 0,
+          outline: 0,
+          padding: 0,
+          margin: 0,
+          cursor: 'pointer',
+          background: 'transparent',
+          ...props.style,
+        }}
+        onMouseMove={props.onMouseMove}
+        onMouseEnter={props.onMouseEnter}
+        onMouseLeave={props.onMouseLeave}>
+        {props.children}
+      </button>
+    );
+  },
+);
