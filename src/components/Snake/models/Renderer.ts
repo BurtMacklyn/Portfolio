c5d9df726b47c364c4c15729fd531aa3f1bc1aa8
cooperrunyan <c@cooperrunyan.com>
2023-09-06 09:02:50 -0700

    Add border to snake

--- src/components/Snake/models/Renderer.ts
+++ src/components/Snake/models/Renderer.ts
@@ -1,4 +1,4 @@
-import { Coordinate } from '../types';
+import { Coordinate, Direction } from '../types';
 import { Clock } from './Clock';
 import { Dimensions } from './Dimensions';
 import { Snake } from './Snake';
@@ -9,8 +9,6 @@ export class Renderer {
   private duration: number;
   private relativeDuration: number;
   private tickSpeed: number;
-
-  public static parent: string = 'body';
 
   private ctx: CanvasRenderingContext2D;
 
@@ -56,9 +54,41 @@ export class Renderer {
     const [ax, ay] = apple.split(':').map(c => +c);
     this.drawBox(ax, ay, this.palette.apple, d);
 
-    for (const segment of snake.slice(lastTail ? 1 : 0, -1)) {
-      const [x, y] = segment.split(':').map(c => +c);
+    const borderedSnake = snake.map((coord, i, a) => {
+      const up = Snake.locate(Direction.Up, coord);
+      const down = Snake.locate(Direction.Down, coord);
+      const left = Snake.locate(Direction.Left, coord);
+      const right = Snake.locate(Direction.Right, coord);
+      const next = a[i + 1];
+      const prev = a[i - 1];
+      let borderUp = true;
+      let borderDown = true;
+      let borderLeft = true;
+      let borderRight = true;
+      if (next === up || prev === up) borderUp = false;
+      if (next === down || prev === down) borderDown = false;
+      if (next === left || prev === left) borderLeft = false;
+      if (next === right || prev === right) borderRight = false;
+      return {
+        coord,
+        borderUp,
+        borderDown,
+        borderLeft,
+        borderRight,
+      };
+    });
+
+    for (const segment of borderedSnake.slice(lastTail ? 1 : 0, -1)) {
+      const [x, y] = segment.coord.split(':').map(c => +c);
       this.drawBox(x, y, this.palette.snake, d);
+      if (segment.borderLeft)
+        this.drawSingleBorder(x, y, Direction.Left, '#000', d);
+      if (segment.borderRight)
+        this.drawSingleBorder(x, y, Direction.Right, '#000', d);
+      if (segment.borderUp)
+        this.drawSingleBorder(x, y, Direction.Up, '#000', d);
+      if (segment.borderDown)
+        this.drawSingleBorder(x, y, Direction.Down, '#000', d);
     }
 
     await Promise.all([
@@ -77,6 +107,8 @@ export class Renderer {
       .split(':')
       .map(x => +x);
 
+    const direction = Snake.reverseLocate(snake.at(-1)!, snake.at(-2)!);
+
     const diffX = headX - beforeHeadX;
     const diffY = headY - beforeHeadY;
 
@@ -88,9 +120,18 @@ export class Renderer {
         this.palette.snake,
         d,
       );
+      if (direction)
+        this.drawBorder(
+          beforeHeadX + diffX * progress,
+          beforeHeadY + diffY * progress,
+          direction,
+          '#000',
+          d,
+        );
     });
 
     this.drawBox(headX, headY, this.palette.snake, d);
+    if (direction) this.drawBorder(headX, headY, direction, '#000', d);
   }
 
   private async animateTail(
@@ -103,11 +144,15 @@ export class Renderer {
       .split(':')
       .map(x => +x);
     const [lastTailX, lastTailY] = lastTail.split(':').map(x => +x);
+    const next = snake.at(1)!;
+
+    const direction = Snake.reverseLocate(snake.at(0)!, next)!;
 
     const diffX = lastTailX - tailX;
     const diffY = lastTailY - tailY;
 
     this.drawBox(tailX, tailY, this.palette.snake, d);
+    this.drawBorder(tailX, tailY, direction, '#000', d);
 
     await this.loop(i => {
       const progress = i / this.frameAmt;
@@ -123,6 +168,13 @@ export class Renderer {
         this.palette.snake,
         d,
       );
+      this.drawBorder(
+        lastTailX - diffX * progress,
+        lastTailY - diffY * progress,
+        direction,
+        '#000',
+        d,
+      );
     });
   }
 
@@ -134,6 +186,59 @@ export class Renderer {
       d.widthUnit,
       d.heightUnit,
     );
+  }
+
+  private drawBorder(
+    x: number,
+    y: number,
+    direction: Direction,
+    color: string,
+    d: Dimensions['state'],
+  ) {
+    this.ctx.lineWidth = 1;
+    this.ctx.strokeStyle = color;
+    if (direction !== Direction.Right) {
+      this.ctx.moveTo(x * d.widthUnit, y * d.heightUnit);
+      this.ctx.lineTo(x * d.widthUnit, (y + 1) * d.heightUnit);
+    }
+    if (direction !== Direction.Left) {
+      this.ctx.moveTo((x + 1) * d.widthUnit, y * d.heightUnit);
+      this.ctx.lineTo((x + 1) * d.widthUnit, (y + 1) * d.heightUnit);
+    }
+    if (direction !== Direction.Up) {
+      this.ctx.moveTo(x * d.widthUnit, (y + 1) * d.heightUnit);
+      this.ctx.lineTo((x + 1) * d.widthUnit, (y + 1) * d.heightUnit);
+    }
+    if (direction !== Direction.Down) {
+      this.ctx.moveTo(x * d.widthUnit, y * d.heightUnit);
+      this.ctx.lineTo((x + 1) * d.widthUnit, y * d.heightUnit);
+    }
+    this.ctx.stroke();
+  }
+
+  private drawSingleBorder(
+    x: number,
+    y: number,
+    direction: Direction,
+    color: string,
+    d: Dimensions['state'],
+  ) {
+    this.ctx.lineWidth = 1;
+    this.ctx.strokeStyle = color;
+    if (direction === Direction.Left) {
+      this.ctx.moveTo(x * d.widthUnit, y * d.heightUnit);
+      this.ctx.lineTo(x * d.widthUnit, (y + 1) * d.heightUnit);
+    } else if (direction === Direction.Right) {
+      this.ctx.moveTo((x + 1) * d.widthUnit, y * d.heightUnit);
+      this.ctx.lineTo((x + 1) * d.widthUnit, (y + 1) * d.heightUnit);
+    } else if (direction === Direction.Down) {
+      this.ctx.moveTo(x * d.widthUnit, (y + 1) * d.heightUnit);
+      this.ctx.lineTo((x + 1) * d.widthUnit, (y + 1) * d.heightUnit);
+    } else if (direction === Direction.Up) {
+      this.ctx.moveTo(x * d.widthUnit, y * d.heightUnit);
+      this.ctx.lineTo((x + 1) * d.widthUnit, y * d.heightUnit);
+    }
+    this.ctx.stroke();
   }
 
   private clearBox(x: number, y: number, d: Dimensions['state']) {
